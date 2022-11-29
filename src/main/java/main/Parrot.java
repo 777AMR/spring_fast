77df9d94db12b042c51532e1201f4f7ff8f43ed6
777AMR <meyram.ramazanov@gmail.com>
2022-11-29 20:47:57 +0600

    Внедрение значений через поля класса с использованием аннотации @Autowired

--- src/main/java/main/Parrot.java
+++ src/main/java/main/Parrot.java
@@ -4,11 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Parrot {
-    private String name;
-
-    public Parrot() {
-        System.out.println("Parrot created");
-    }
+    private String name = "Koko";
 
     public String getName() {
         return name;
